ad8e5c55385ee9d12ff287c739a34f1caaecb81f
Muhammad Sani Haruna <muhammadsaniharuna44@gmail.com>
2022-08-11 09:24:06 +0100

    +setting up Sanity API

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import styled from "styled-components";  
+import { Link } from 'react-router-dom';
 import Header from './Header'
 import client from '../client';
 
@@ -13,6 +14,14 @@ const Showcase = styled.div`
    justify-content: center;
    align-items: center;
 `
+const Flex = styled.div`
+   margin: auto;
+   max-width: 1200px;
+   width: 90%;
+   display: grid;
+   grid-template-columns: 1fr 1fr 1fr;
+   grid-gap: 30px;
+`
 
 const Blog = () => {
 
@@ -20,10 +29,10 @@ const Blog = () => {
 
   useEffect(()=>{
       client.fetch(
-          `*[_type == 'posts' ]{
+          `*[_type == 'post' ]{
               title,
-              body,
               slug,
+              body,
               mainImage{
                   asset -> {
                       id,
@@ -33,21 +42,27 @@ const Blog = () => {
               }
           }`
       ) .then((data)=> setPosts(data)).catch(console.error)
-
-
   }, [])
-
   return (
-    <Container>
+    <section>
+       <Container>
     <Header />
     <Showcase>
         <h1 style={{fontSize: '55px', fontWeight: '700', color: '#1b1b1b'}}>UNIS BLOG</h1>
        <p style={{color: '#1b1b1b',fontWeight: '500'}}>Stay Updated on our new releases and other industry buzz.</p>
     </Showcase>
-    <h2>You are viewing {posts.map((post)=>
-      console.log(post)
-    )} posts</h2>
+    <h2>You are viewing {posts.length} posts</h2>
     </Container>
+    <Flex>
+      {posts.map(post => 
+         <article  key={post.slug.current}>
+           <img style={{width: '380px', height: '200px', objectFit: 'cover'}} src={post.mainImage.asset.url} alt={post.title}/>
+           <h4>{post.title}</h4> 
+           <Link to={`/blog/${post.slug.current}`}>Read Full Article</Link>
+         </article>      
+        )}
+    </Flex>
+    </section>
   )
 }
 
